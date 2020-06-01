e6164651eb1e071a7000ec378c0ac3ce0011e683
Chih-Hsuan Huang <chhuang215@gmail.com>
2020-06-01 23:14:10 +0800

    init commit

---
+++ WebSite1/SmsEmail.aspx.vb
@@ -0,0 +1,21 @@
+﻿
+Partial Class Default2
+    Inherits System.Web.UI.Page
+
+
+    'Private Sub mv_Load(sender As Object, e As EventArgs) Handles mv.Load
+    '    mv.SetActiveView(viewList)
+    'End Sub
+
+    'Protected Sub lbtnList_Click(sender As Object, e As EventArgs) Handles lbtnList.Click
+    '    mv.SetActiveView(viewList)
+    'End Sub
+    'Protected Sub lbtnCreateTemplate_Click(sender As Object, e As EventArgs) Handles lbtnCreateTemplate.Click
+    '    mv.SetActiveView(viewCreateTemplate)
+    'End Sub
+    'Protected Sub lbtnUploadAndSend_Click(sender As Object, e As EventArgs) Handles lbtnUploadAndSend.Click
+    '    mv.SetActiveView(viewUploadAndSend)
+    'End Sub
+
+
+End Class
