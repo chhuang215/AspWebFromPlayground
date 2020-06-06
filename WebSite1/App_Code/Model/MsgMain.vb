7c0d7e6bc9a2132e44a03998e8b27528069520f9
Chih-Hsuan Huang <chhuang215@gmail.com>
2020-06-06 16:43:10 +0800

    test gridview and model class interaction

---
+++ WebSite1/App_Code/Model/MsgMain.vb
@@ -0,0 +1,14 @@
+﻿Imports Microsoft.VisualBasic
+
+Public Class MsgMain
+
+    Public Property ID() As Integer
+    Public Property Message() As String
+    Public Property CreateDate() As Date
+    Public Sub New()
+        ID = -1
+        Message = String.Empty
+        CreateDate = Nothing
+    End Sub
+
+End Class
--- WebSite1/SmsEmail.aspx.vb
+++ WebSite1/SmsEmail.aspx.vb
@@ -1,8 +1,27 @@
 ﻿
+Imports System.Data
+
 Partial Class Default2
     Inherits System.Web.UI.Page
 
+    Public Sub PageInit() Handles Me.Init
 
+        Dim dt As DataTable = New DataTable()
+
+        Dim lstMsgs As List(Of MsgMain) = New List(Of MsgMain)
+
+        For i As Integer = 1 To 5
+            Dim mm As New MsgMain
+            mm.ID = i
+            mm.Message = "Msg " & i
+            mm.CreateDate = Date.Now
+            lstMsgs.Add(mm)
+        Next
+
+        gvMsgTemplates.DataSource = lstMsgs
+        gvMsgTemplates.DataBind()
+
+    End Sub
     'Private Sub mv_Load(sender As Object, e As EventArgs) Handles mv.Load
     '    mv.SetActiveView(viewList)
     'End Sub
